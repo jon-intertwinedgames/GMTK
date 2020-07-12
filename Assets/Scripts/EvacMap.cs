f4a7dd68f7f0f7d1f24410da87b8bbf594373dd7
Charles Fernandez <charliefernandez777@gmail.com>
2020-07-12 09:58:13 -0400

    Map can be toggled on and off

---
+++ Assets/Scripts/EvacMap.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvacMap : MonoBehaviour
+{
+    public void ToggleMap()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+}
